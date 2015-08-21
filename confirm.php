f310ef4e74c68d385b6ed4afbda92065967add7e
Takeuchi Kazuya <takeuchi@tam-tam.co.jp>
2015-08-21 18:26:08 +0900

    【mod】html

--- confirm.php
+++ confirm.php
@@ -1,2 +1,11 @@
-<p><?php echo htmlspecialchars($_POST['name']); ?></p>
-<p><?php echo htmlspecialchars($_POST['comment']);?></p>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php echo htmlspecialchars($_POST['name']); ?></p>
+    <p><?php echo htmlspecialchars($_POST['comment']);?></p>
+</body>
+</html>
--- input.php
+++ input.php
@@ -1,13 +1,22 @@
-<form action="confirm.php" method="post">
-    <dl>
-        <dt>名前</dt>
-        <dd><input type="text" name="name"></dd>
-    </dl>
-    <dl>
-        <dt>コメント</dt>
-        <dd><textarea name="comment" rows="4" cols="40"></textarea></dd>
-    </dl>
-    <div>
-        <input type="submit" value="送信">
-    </div>
-</form>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form action="confirm.php" method="post">
+        <dl>
+            <dt>名前</dt>
+            <dd><input type="text" name="name"></dd>
+        </dl>
+        <dl>
+            <dt>コメント</dt>
+            <dd><textarea name="comment" rows="4" cols="40"></textarea></dd>
+        </dl>
+        <div>
+            <input type="submit" value="送信">
+        </div>
+    </form>
+</body>
+</html>
